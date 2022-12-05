6d85d7e4fdb9d666ec4e6e94e6b84e69c9a3a872
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-12-05 15:45:19 -0600

    Minor change: Setup to ignore web code
    This is the way the project should be for development purposes. It is
    much faster to compile without Actix.
    
    Contact: Sam Christy <samchristywork@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use env_logger::Env;
 
 mod get_json;
+mod wave_function_collapse;
 mod web;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
 
+    wave_function_collapse::foo();
 
-    web::serve().await
+    //web::serve().await
+
+    Ok(())
 }
